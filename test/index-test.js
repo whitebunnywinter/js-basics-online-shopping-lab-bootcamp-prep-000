e75182718b74c12a484cdf51f4a5bad55060d301
Haruka Matsumoto <matsumoto.haru@gmail.com>
2017-12-25 02:25:17 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -88,7 +88,7 @@ describe("viewCart()", function() {
     const lemonsCost = getCart()[0]["lemons"];
 
     var str = viewCart();
-    expect(str).toBe(`In your cart, you have lemons at $${lemonsCost}`);
+    expect(str).toBe(`In your cart, you have lemons at $${lemonsCost}.`);
     expect(console.log).toHaveBeenCalledWith(
       `In your cart, you have lemons at $${lemonsCost}.`
     );
