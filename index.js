2375dd01d29ad1c93e980b9bcf2bd3a925ab2368
Haruka Matsumoto <matsumoto.haru@gmail.com>
2017-12-25 02:19:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function viewCart() {
     for (var i = 0; i < cart.length; i++) {
       var keyNames = Object.keys(cart[i]);
       var price = cart[i].keyNames;
-      str += `${keyNames} at $${cart[i][keyNames]}.`;
+      str += `${keyNames} at $${price}.`;
     }
     console.log(str);
     return str;
