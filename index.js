9eff635e1b8380cb72c63324e3841ad1284020a6
Haruka Matsumoto <matsumoto.haru@gmail.com>
2017-12-25 03:55:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -78,9 +78,10 @@ function placeOrder(cardNumber) {
   if (cardNumber) {
     var totalPrice = total();
     console.log(`Your total cost is $${totalPrice}, which will be charged to the card ${cardNumber}.`);
+    cart= [];
   } else {
     console.log("Sorry, we don't have a credit card on file for you.");
-    cart= [];
+    
   }
 }
 
