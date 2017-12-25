5195c025dbb6218f62a81ccee1e4f2d10e9f0739
Haruka Matsumoto <matsumoto.haru@gmail.com>
2017-12-25 02:53:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,7 +69,8 @@ function removeFromCart(item) {
       return cart;
     }
   }
-  return 'That item is not in your cart.';
+  console.log('That item is not in your cart.');
+  return cart;
 }
 
 function placeOrder(cardNumber) {
