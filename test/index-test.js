9cc4b6eb74e559f98c2a199bc302ffee2e921139
Haruka Matsumoto <matsumoto.haru@gmail.com>
2017-12-25 02:31:04 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -118,7 +118,6 @@ describe("viewCart()", function() {
     const quinceCost = getCart()[2]["quince"];
 
     var str = viewCart();
-    console.log(str);
     expect(console.log).toHaveBeenCalledWith(
       `In your cart, you have orange at $${orangeCost}, pear at $${pearCost}, and quince at $${quinceCost}.`
     );
